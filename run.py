6490cce321c11c9dbfe7d756a935414b015a85ee
Johnny Chang <66932108+hiimdoublej-swag@users.noreply.github.com>
2022-07-21 17:01:13 +0800

    Add volume mounts and another case

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ if __name__ == '__main__':
     print('Runner started...')
     while True:
         time.sleep(0.2)
-        app.outer.apply_async().get()
+
+        # Both of these would trigger the bug
+        # app.outer.apply_async().get()
+        app.outer.apply_async()
